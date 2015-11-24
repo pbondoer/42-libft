d3f69493840020905898209fdfc65ba4f41d4dda
Pierre BONDOERFFER <pbondoer@e1r12p9.42.fr>
2015-11-24 23:58:41 +0100

    dump 23:58 zzzz

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbondoer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/24 19:59:25 by pbondoer          #+#    #+#             */
+/*   Updated: 2015/11/24 23:27:51 by pbondoer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_t len);
+#endif
