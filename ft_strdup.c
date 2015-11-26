d2ea7f69f5f1e672ccc77c9ccbbaeae5c045d993
Pierre Bondoerffer <pbondoer@student.42.fr>
2015-11-26 16:51:48 +0100

    more stuff

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 14:53:35 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/11/26 15:02:09 by pbondoer         ###   ########.fr       */
+/*   Updated: 2015/11/26 16:50:25 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,10 @@ char	*ft_strdup(const char *s1)
 	size_t	len;
 	char	*s2;
 
-	len = ft_strlen(s1);
+	len = ft_strlen(s1) + 1;
 	s2 = (char *)malloc(sizeof(char) * len);
 	if (s2 == NULL)
 		return (NULL);
-	while (len >= 0)
-	{
-		s2[len] = s1[len];
-		len--;
-	}
+	ft_memcpy(s2, s1, len);
 	return (s2);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pbondoer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 19:59:25 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/11/26 14:54:06 by pbondoer         ###   ########.fr       */
+/*   Updated: 2015/11/26 16:44:36 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,8 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t n);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 #endif
