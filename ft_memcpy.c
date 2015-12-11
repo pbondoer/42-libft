6b986dfd7c18d0f003e23ab988f874d33b055a3e
pbondoer <pbondoer@student.42.fr>
2015-12-11 22:46:38 +0100

    memcpy: use unsigned for consistency

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 22:27:35 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/12/01 21:48:57 by pbondoer         ###   ########.fr       */
+/*   Updated: 2015/12/11 22:46:15 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	while (n > 0)
 	{
 		n--;
-		((char *)dst)[n] = ((char *)src)[n];
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
 	}
 	return (dst);
 }
