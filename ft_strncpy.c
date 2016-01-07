e1d2a454cb5e1d9ca0d5e2126fa82cc79cf10acb
pbondoer <pbondoer@student.42.fr>
2016-01-07 21:33:52 +0100

    ft_strncpy: implement

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 15:20:21 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/12/02 00:39:19 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/01/07 21:33:40 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t n)
 {
-	size_t len;
+	int i;
 
-	len = ft_strlen(src);
-	if (len > n)
-		len = n;
-	ft_memcpy(dst, src, len);
-	ft_bzero(dst + len, n - len);
+	i = 0;
+	while(src[i] && i < n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while(i < n)
+	{
+		dst[i] = '\0';
+		i++;
+	}
 	return (dst);
 }
