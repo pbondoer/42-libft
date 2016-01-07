5505e7968041ebdeca23a42a8b6ca5d53d2a5cd1
pbondoer <pbondoer@student.42.fr>
2016-01-07 21:32:01 +0100

    ft_strcpy: implement strcpy

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,13 +6,20 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 15:24:24 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/12/04 02:12:53 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/01/07 21:16:02 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 char	*ft_strcpy(char *dst, const char *src)
 {
-	return (ft_strncpy(dst, src, ft_strlen(src) + 1));
+	int i;
+
+	i = 0;
+	while(src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
