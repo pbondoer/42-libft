93b7878ba87d4cc5a0704213d95df5e065d16060
pbondoer <pbondoer@student.42.fr>
2016-01-08 01:44:38 +0100

    ft_strncmp: implement

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,15 +6,23 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 00:42:52 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/12/03 23:24:46 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/01/08 01:44:03 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <string.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	return (ft_memcmp(s1, s2, ft_min(n, ft_min(ft_strlen(s1) + 1,
-						ft_strlen(s2) + 1))));
+	while (n > 0)
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		else if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
+		n--;
+	}
+	return (0);
 }
