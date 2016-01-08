838bfea96a2003cc9aeabe8ec63f60f1a72923a6
pbondoer <pbondoer@student.42.fr>
2016-01-08 01:02:25 +0100

    ft_strstr: implement

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 00:41:10 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/12/02 00:42:21 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/01/08 01:02:04 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,21 @@
 
 char	*ft_strstr(const char *s1, const char *s2)
 {
-	return (ft_strnstr(s1, s2, ft_strlen(s1)));
+	char *a;
+	char *b;
+
+	while (*s1)
+	{
+		a = s1;
+		b = s2;
+		while (*b && *a == *b)
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+			return s1;
+		s1++;
+	}
+	return (NULL);
 }
