32b5f04f5b52fac021b5dd640a8c1b057ad8f078
Pierre BONDOERFFER <pbondoer@student.42.fr>
2016-02-21 14:32:18 +0100

    ft_strmerge: implement

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 19:59:25 by pbondoer          #+#    #+#             */
-/*   Updated: 2016/02/06 19:13:29 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/02/21 14:14:50 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ char			*ft_strstr(const char *s1, const char *s2);
 char			*ft_strnstr(const char *s1, const char *s2, size_t n);
 int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
+char			*ft_strmerge(char *s1, char *s2);
 int				ft_atoi(const char *str);
 void			*ft_memalloc(size_t size);
 void			ft_memdel(void **ap);
