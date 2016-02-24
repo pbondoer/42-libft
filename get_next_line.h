dd729d09afdb007773b969ca0f6998c11437774d
Pierre BONDOERFFER <pbondoer@student.42.fr>
2016-02-24 13:20:40 +0100

    getlines: oops, header

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 19:11:56 by pbondoer          #+#    #+#             */
-/*   Updated: 2016/02/23 00:34:39 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/02/23 07:58:46 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 4096
+# include "libft.h"
 
 typedef struct	s_gnl
 {
