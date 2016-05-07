993526c758f939302d7bade8ee186c001ee12b0b
Pierre Bondoerffer <pierre@bondoer.fr>
2016-05-07 13:16:55 +0200

    linux compatibility fix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 22:48:36 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/12/07 20:36:32 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/04/30 12:38:44 by lemon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 		((char *)dst)[i] = ((char *)src)[i];
 		i++;
 		if (((unsigned char *)dst)[i - 1] == cc)
-			return (&dst[i]);
+			return (&((unsigned char *)dst)[i]);
 	}
 	return (NULL);
 }
--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 23:19:39 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/12/11 22:37:57 by pbondoer         ###   ########.fr       */
+/*   Updated: 2016/04/30 12:39:08 by lemon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return (((void *)&s[i]));
+			return (((void *)&((unsigned char *)s)[i]));
 		i++;
 	}
 	return (NULL);
