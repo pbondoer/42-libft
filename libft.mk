be0806e1bac31cb57fb26a83efb61dfefd0950c3
Pierre BONDOERFFER <pbondoer@student.42.fr>
2017-01-02 10:54:31 +0100

    add global make variables

--- libft.mk
+++ libft.mk
@@ -6,7 +6,7 @@
 #    By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/12/31 05:24:55 by pbondoer          #+#    #+#              #
-#    Updated: 2016/12/31 06:50:01 by pbondoer         ###   ########.fr        #
+#    Updated: 2017/01/02 10:54:17 by pbondoer         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,3 +23,6 @@ FT_NAME		= libft.a
 FT_LNK		= -L $(L_FT) -l ft
 FT_INC		= -I $(L_FT)/includes
 FT_LIB		= $(L_FT)/$(FT_NAME)
+
+LNK			+= $(FT_LNK)
+INC			+= $(FT_INC)
