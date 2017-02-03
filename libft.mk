2778c8c39e8411063e3cfe045c3fb00d50ed171a
Pierre BONDOERFFER <pbondoer@student.42.fr>
2017-02-03 03:00:30 +0100

    better solution to this, prevents recursive var

--- libft.mk
+++ libft.mk
@@ -6,7 +6,7 @@
 #    By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/12/31 05:24:55 by pbondoer          #+#    #+#              #
-#    Updated: 2017/01/02 10:55:55 by pbondoer         ###   ########.fr        #
+#    Updated: 2017/02/03 02:58:40 by pbondoer         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,15 +14,20 @@
 # Use with 'include /path/to/libft.mk'
 # Make sure to define L_FT with path to libft first
 
-ifndef L_FT
-	L_FT	= .
+L_FT		?= .
+
+FT_NAME		:= libft.a
+
+FT_LNK		:= -L $(L_FT) -l ft
+FT_INC		:= -I $(L_FT)/includes
+FT_LIB		:= $(L_FT)/$(FT_NAME)
+
+# Global variables for compilation
+
+ifndef LIB_LNK
+LIB_LNK		:=
+LIB_INC		:=
 endif
-
-FT_NAME		= libft.a
-
-FT_LNK		= -L $(L_FT) -l ft
-FT_INC		= -I $(L_FT)/includes
-FT_LIB		= $(L_FT)/$(FT_NAME)
 
 LIB_LNK		+= $(FT_LNK)
 LIB_INC		+= $(FT_INC)
