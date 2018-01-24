6edaddd43bb17f41b50ea65ea6340750d2c0a4b3
Pierre BONDOERFFER <pbondoer@student.42.fr>
2018-01-24 17:53:40 +0100

    fix an include

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 19:59:25 by pbondoer          #+#    #+#             */
-/*   Updated: 2017/12/14 14:17:08 by pbondoer         ###   ########.fr       */
+/*   Updated: 2018/01/24 17:53:17 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,7 +99,6 @@ float			ft_clamp(float i, float a, float b);
 void			ft_hsort(void *base, size_t num, size_t size,
 					int (*compar)(const void *, const void *));
 
-{
 typedef struct	s_array
 {
 	void		*ptr;
